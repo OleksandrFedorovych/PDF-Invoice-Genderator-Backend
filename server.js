ff22222ddcf9794a0e49ae1cc864dd5a0657f35f
OleksandrFedorovych <kornelkarnel@gmail.com>
2025-08-06 01:53:42 -0700

    fix custom value

--- server.js
+++ server.js
@@ -16,25 +16,75 @@ app.post("/generate", async (req, res) => {
     const pdfDoc = await PDFDocument.load(existingPdfBytes);
     const form = pdfDoc.getForm();
 
+    form.getTextField("seller_full_name").setText($(`${data.seller_name_1}` `${data.seller_name_2}`));
+    form.getTextField("buyer_full_name").setText(`Adir Schneider PA`);
     form.getTextField("property_street_address").setText(`${data.property_street_address}, ${data.property_city}, ${data.property_state}`);
-    form.getTextField("seller_full_name").setText(`${data.seller_name_1} ${data.seller_name_2}`);
-    form.getTextField("buyer_full_name").setText(`${data.buyer_name}`);
     form.getTextField("property_county").setText(`${data.property_county}`);
     form.getTextField("property_tax_id").setText(`${data.property_tax_id}`);
     form.getTextField("legal_description").setText(`${data.legal_description}`);
+    form.getTextField("other_personal_property").setText(``); // ??
+    form.getTextField("excluded_items").setText(``); // ??
     form.getTextField("purchase_price").setText(`${data.purchase_price}`);
     form.getTextField("initial_deposit_amount").setText(`${data.initial_deposit_amount}`);
-    form.getTextField("listing_agent_name").setText(`${data.list_agent_name}`);
-    form.getTextField("listing_agent_phone").setText(`${data.list_agent_phone}`);
-    form.getTextField("listing_agent_email").setText(`${data.list_agent_email}`);
-    form.getTextField("listing_agent_address").setText(`${data.list_agent_address}`);
-    form.getTextField("listing_agent_fax").setText(`${data.list_agent_fax}`);
-    form.getTextField("effective_date").setText(`${data.effective_date}`);
+    form.getTextField("escrow_agent_name").setText(`Cooperative Title Agency of Florida, lnc.`);
+    form.getTextField("escrow_agent_address").setText(`9700 Griffin Road Cooper City FL 33328`);
+    form.getTextField("escrow_agent_phone").setText(`(954) 616-5302`);
+    form.getTextField("escrow_agent_email").setText(`richard@cooptitle.com`);
+    form.getTextField("escrow_agent_fax").setText(``);
+
+    form.getTextField("additional_deposit_delay_days").setText(`10`);
+    form.getTextField("additional_deposit_amount").setText(``);
+
+    form.getTextField("loan_amount").setText(``);
+    form.getTextField("other_closing_funds_description").setText(``);
+    form.getTextField("other_closing_funds").setText(``);
+
+    form.getTextField("balance_to_close").setText(`${data.balance_to_close}`);
+    form.getTextField("offer_acceptance_deadline").setText(`${data.offer_acceptance_deadline}`);
     form.getTextField("closing_date").setText(`${data.closing_date}`);
-    form.getTextField("buyer_name_1").setText(`${data.buyer_name_1}`);
-    form.getTextField("buyer_name_2").setText(`${data.buyer_name_2}`);
-    form.getTextField("seller_name_1").setText(`${data.seller_name_1}`);
-    form.getTextField("seller_name_2").setText(`${data.seller_name_2}`);
+
+    form.getTextField("buyer_initial_1").setText(`${data.first_name}`);
+    form.getTextField("buyer_initial_2").setText(`${data.last_name}`);
+    form.getTextField("seller_initial_1").setText(`${data.seller_name_1}`);
+    form.getTextField("seller_initial_2").setText(`${data.seller_name_2}`);
+
+    form.getTextField("loan_approval_period_days").setText(`30`);
+    form.getTextField("loan_type_other").setText(``);
+    form.getTextField("loan_interest_rate_cap").setText(``);
+    form.getTextField("loan_term_years").setText(`30`);
+    form.getTextField("loan_application_deadline_days").setText(`5`);
+
+    form.getTextField("buyer_other_costs").setText(``);
+    form.getTextField("title_evidence_deadline_days").setText(``);
+    form.getTextField("title_evidence_deadline_days").setText(`15`);
+
+    form.getTextField("title_search_max_cost").setText(`200`);
+    form.getTextField("home_warranty_provider").setText(``);
+    form.getTextField("home_warranty_cost_cap").setText(``);
+    
+    form.getTextField("flood_termination_days").setText(`20`);
+
+    form.getTextField("inspection_period_days").setText(`15`);
+
+    form.getTextField("other_terms").setText(``);
+    form.getTextField("additional_terms").setText(`- Seller shalle convey and transfer clear and marketable title, free and clear from any encumbrances, clouds, \n title, defects, open, active, expired or inactive permits, county or municipal violations liens or other defects. \n buyer is a licensed real estate agent \n
+    - Seller shall provide access to the property with 24 hours notice.`);
+
+    form.getTextField("buyer1").setText(`?`);
+    form.getTextField("buyer2").setText(`?`);
+    form.getTextField("seller1").setText(`?`);
+    form.getTextField("seller2").setText(`?`);
+    form.getTextField("buyer1_date").setText(`?`);
+    form.getTextField("buyer2_date").setText(`?`);
+    form.getTextField("seller1_date").setText(`?`);
+    form.getTextField("seller2_date").setText(`?`);
+    form.getTextField("buyer_address_notice").setText(`?`);
+    form.getTextField("seller_address_notice").setText(`?`);
+    form.getTextField("cooperate_sales_associate").setText(`?`);
+    form.getTextField("cooperate_broker").setText(`?`);
+    form.getTextField("list_sales_associat").setText(`?`);
+    form.getTextField("list_broker").setText(`?`);
+
 
     form.flatten();
     console.log("✅ Generate PDF Start");
